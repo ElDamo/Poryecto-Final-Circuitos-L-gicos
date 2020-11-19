d076573e3b5d686a4190edfe2c6f2c3899872bb9
Israel <ifonsecap351@gmail.com>
2020-11-18 23:54:45 -0600

    Data Memory

--- DataMemory/DataMemory.vhd
+++ DataMemory/DataMemory.vhd
@@ -6,13 +6,28 @@ use ieee.numeric_std.all;
 
 entity DataMemory is
 	port(
-	
+		MB: In std_logic_vector(0 to 15);
+		SA: In std_logic_vector(0 to 2);
+		clk: in std_logic;
+		MW: in std_logic;--1 escribe MB en la dirección SA
+		RD: out std_logic_vector(0 to 15) --lee la dirección SA en RD
 	);
 	
 end DataMemory;
 
 architecture behaivour of DataMemory is
+type dataMemory is array(0 to 7) of std_logic_vector(0 to 15);
+signal data : dataMemory;
 begin
 
+process(clk)
+begin
+	if(clk'event and clk='1') then
+		if(MW='1') then
+			data(to_integer(unsigned(SA)))<=MB;
+		end if;
+	end if;
+	RD<=data(to_integer(unsigned(SA)));
+end process;
 
 end behaivour;
