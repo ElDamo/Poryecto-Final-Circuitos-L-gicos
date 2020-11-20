f225288c93323fede32b87647233304d534227c2
juan manuel oceguera urquiza <joceguer@itam.mx>
2020-11-20 13:09:30 -0600

    se agregó el contador

--- ProgramCounter/ProgramCounter.vhd
+++ ProgramCounter/ProgramCounter.vhd
@@ -18,6 +18,7 @@ entity ProgramCounter is
 		memory5 : in std_logic_vector(15 downto 0);
 		memory6 : in std_logic_vector(15 downto 0);
 		memory7 : in std_logic_vector(15 downto 0);
+		contador : out std_logic_vector(7 downto 0) := "00000000";
 		DR : out std_logic_vector(2 downto 0);
 		memory_out : out std_logic_vector(15 downto 0)
 	
@@ -26,11 +27,14 @@ entity ProgramCounter is
 end ProgramCounter;
 
 architecture behaivour of ProgramCounter is
+
+signal contador_aux : std_logic_vector(7 downto 0) := "00000000";
+
 begin
 
 DR <= instruccion(8 downto 6);
 
-	process(clk)
+	process(clk, instruccion)
 		begin
 		if (clk'event and clk = '1') then
 			if instruccion(15 downto 14) = "10" then --comprobamos si es immediate
@@ -46,12 +50,19 @@ DR <= instruccion(8 downto 6);
 										when "110" => memory_out <= (memory6 + instruccion(2 downto 0));
 										when "111" => memory_out <= (memory7 + instruccion(2 downto 0));
 					end case;
-					
+					contador_aux <= contador_aux + 1;
 				elsif instruccion(11) = '1' then --sabemos que es load immediate
 					memory_out(15 downto 3) <= "0000000000000";
 					memory_out(2 downto 0) <= instruccion(2 downto 0);
+					contador_aux <= contador_aux + 1;
 				end if;
 			end if;
+			--aqui es donde tiene que ir el branch, jump, load y store, y la unidad funcional
 		end if;
+	end process;
+	
+	process(contador_aux)
+	begin
+		contador <= contador_aux;
 	end process;
 end behaivour;
