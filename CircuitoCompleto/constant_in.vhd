e51311f85a2739d2d7526d5514c12592aa38d235
adalbert01997 <adalbert01997@hotmail.com>
2020-11-25 18:53:31 -0600

    Se añadio Constant in

--- CircuitoCompleto/constant_in.vhd
+++ CircuitoCompleto/constant_in.vhd
@@ -8,13 +8,16 @@ use ieee.std_logic_1164.all;
 entity constant_in is
 	
 	port(
-				G : out std_logic_vector(0 to 15));
+				G : out std_logic_vector(0 to 15);
+				B_address : in std_logic_vector(0 to 2));
 				
 end constant_in;
 
 
 architecture behavior of constant_in is
+signal res: std_logic_vector(16 downto 0) := "00000000000000000";
 	begin
-	G <= "1010101111001101";
+	res <= "0000000000000"+B_address;
+	G <=res(15 downto 0); 
 
 end behavior;
